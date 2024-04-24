8564b4f235a5d3fee2b4afb7573018b66fd6c2ba
yohar <flameyeelf@gmail.com>
2024-04-24 21:39:53 +0900

    add write OBJ

--- include/ArmorerPhys/os.h
+++ include/ArmorerPhys/os.h
@@ -4,6 +4,23 @@
 #include <string>
 #include <vector>
 
-namespace aphys {}  // namespace aphys
+#include <igl/writeOBJ.h>
+
+namespace aphys {
+
+inline void writeOBJ(const std::string &filename, const MatxXf &V,
+                     const MatxXi &F) {
+  if (V.cols() == 2) {
+    MatxXf V3(V.rows(), 3);
+    V3 << V, MatxXf::Zero(V.rows(), 1);
+    igl::writeOBJ(filename, V3, F);
+  } else if (V.cols() == 3) {
+    igl::writeOBJ(filename, V, F);
+  } else {
+    throw std::runtime_error("writeOBJ: V must have 2 or 3 columns");
+  }
+  igl::writeOBJ(filename, V, F);
+}
+}  // namespace aphys
 
 #endif  // GL_OS_H_
