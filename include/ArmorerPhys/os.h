dee93808d98a26e18e720dd5032deb5aa8818b7d
yohar <flameyeelf@gmail.com>
2024-04-24 21:46:09 +0900

    fix writeobj

--- include/ArmorerPhys/os.h
+++ include/ArmorerPhys/os.h
@@ -8,19 +8,18 @@
 
 namespace aphys {
 
-inline void writeOBJ(const std::string &filename, const MatxXf &V,
+inline void writeOBJ(const std::string &filename, const MatxXd &V,
                      const MatxXi &F) {
   if (V.cols() == 2) {
-    MatxXf V3(V.rows(), 3);
-    for (int i = 0; i < V.rows(); i++) {
-      V3.row(i) << V.row(i), 0.0;
-    }
+    MatxXd V3(V.rows(), 3);
+    V3 << V, MatxXd::Zero(V.rows(), 1);
     igl::writeOBJ(filename, V3, F);
   } else if (V.cols() == 3) {
     igl::writeOBJ(filename, V, F);
   } else {
     throw std::runtime_error("writeOBJ: V must have 2 or 3 columns");
   }
+  igl::writeOBJ(filename, V, F);
 }
 }  // namespace aphys
 
