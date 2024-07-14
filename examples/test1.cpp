b92245576477090970ffd1615f16f28057c06228
Yoharol <flameyeelf@gmail.com>
2024-07-14 14:47:28 +0900

    update

--- examples/test1.cpp
+++ examples/test1.cpp
@@ -14,6 +14,17 @@
 const unsigned int SCR_WIDTH = 700;
 const unsigned int SCR_HEIGHT = 700;
 
+double cubicSplineKernel(double d, double h) {
+  double q = d / h;
+  double W = 0.0;
+
+  if (q >= 0 && q <= 1) {
+    W = 1 - 3 * std::pow(q, 2) + 2 * std::pow(q, 3);
+  }
+
+  return W;
+}
+
 int main() {
   GLFWwindow* window = aphys::create_window(
       SCR_WIDTH, SCR_HEIGHT, "Example6: interactive pbd simulator");
@@ -91,40 +102,6 @@ int main() {
   aphys::MatxXd dx = v_p;
   aphys::MatxXd residual_force = v_p;
 
-  // ===================== prepare render data =====================
-  aphys::Points points = aphys::create_points();
-  points.point_size = 3.0f;
-
-  aphys::Points handle_points = aphys::create_points();
-  handle_points.color = aphys::RGB(0, 0, 0);
-  handle_points.point_size = 5.0f;
-
-  aphys::Edges edges = aphys::create_edges();
-  edges.color = aphys::RGB(255, 0, 0);
-  edges.width = 0.5f;
-
-  aphys::Lines force_lines = aphys::create_deriv_lines();
-  force_lines.color = aphys::RGB(220, 0, 131);
-  force_lines.width = 1.0f;
-
-  aphys::set_points_data(points, v_p.cast<float>(), aphys::MatxXf());
-  aphys::set_edges_data(edges, v_p.cast<float>(), edge_indices,
-                        aphys::MatxXf());
-  aphys::set_points_data(handle_points, handles.cast<float>(), aphys::MatxXf());
-
-  aphys::add_render_func(scene, aphys::get_render_func(handle_points));
-  // aphys::add_render_func(scene, aphys::get_render_func(points));
-  aphys::add_render_func(scene, aphys::get_render_func(force_lines));
-  aphys::add_render_func(scene, aphys::get_render_func(edges));
-
-  // ===================== handle input =====================
-  int curr_mode = 0;
-  aphys::add_gui_key_input_func(gui, [&curr_mode, &handles]() {
-    if (ImGui::IsKeyPressed(ImGuiKey_H)) {
-      curr_mode = (curr_mode + 1) % 2;
-      std::cout << curr_mode << std::endl;
-    }
-  });
   // ===================== lbs =====================
   std::vector<aphys::MatxXd> T;
   for (int i = 0; i < handles.rows(); i++) {
@@ -134,6 +111,7 @@ int main() {
   }
   aphys::MatxXd v_p_rig = v_p;
   aphys::MatxXd handles_rig = handles;
+  aphys::MatxXd A(v_p.rows() * 2, 3);
 
   auto lbs = [&]() {
     for (int i = 0; i < v_p.rows(); i++) {
@@ -141,6 +119,7 @@ int main() {
       for (int j = 0; j < T.size(); j++) {
         t += weights(i, j) * T[j];
       }
+      A.block(i * 2, 0, 2, 3) = t;
       aphys::Vecxd x_tilde(3);
       x_tilde << v_p(i, 0), v_p(i, 1), 1;
       v_p_rig.row(i) = (t * x_tilde).transpose();
@@ -190,6 +169,109 @@ int main() {
   };
   construct_W();
 
+  // ===================== optimize =====================
+  int hp_idx;
+  aphys::MatxXd hp_pos(1, 2);
+  aphys::Matx3f hp_weight_color(v_p.rows(), 3);
+  aphys::MatxXd optimized_rig = v_p_rig;
+  auto optimize_one_step = [&]() {
+    aphys::MatxXd delta_x = dx - v_p_rig;
+    double max_dis = -1.0;
+    for (int i = 0; i < delta_x.rows(); i++) {
+      double dis = (dx.row(i) - v_p_rig.row(i)).squaredNorm();
+      if (dis > max_dis) {
+        max_dis = dis;
+        hp_idx = i;
+      }
+    }
+    hp_pos = v_p_rig.row(hp_idx);
+
+    aphys::Vecxd translate = dx.row(hp_idx) - v_p_ref.row(hp_idx);
+    aphys::MatxXd P(2, 2), Q(2, 2);
+    P.setZero();
+    Q.setZero();
+
+    aphys::Vecxd hp_weights(delta_x.rows());
+
+    for (int i = 0; i < delta_x.rows(); i++) {
+      hp_weights(i) = cubicSplineKernel(
+          (v_p_ref.row(i) - v_p_ref.row(hp_idx)).norm(), 0.30);
+      aphys::Vecxd color = aphys::heat_rgb(hp_weights(i), 0.0, 1.0);
+      hp_weight_color.row(i) = color.cast<float>();
+    }
+
+    for (int i = 0; i < delta_x.rows(); i++) {
+      aphys::Vecxd pi = dx.row(i) - dx.row(hp_idx);
+      aphys::Vecxd qi = v_p_ref.row(i) - v_p_ref.row(hp_idx);
+      double w = hp_weights(i);
+      P += w * pi * qi.transpose();
+      Q += w * qi * qi.transpose();
+    }
+
+    aphys::MatxXd hp_A(2, 2);
+    hp_A = P * Q.inverse();
+    hp_A.conservativeResize(2, 3);
+    hp_A.block(0, 2, 2, 1) = translate;
+
+    std::cout << hp_A << std::endl;
+
+    set_handle_world_affine(hp_A, v_p_ref.row(hp_idx));
+
+    hp_A = 2.0 * hp_A - A.block(hp_idx * 2, 0, 2, 3);
+
+    for (int i = 0; i < delta_x.rows(); i++) {
+      double weight_sum = 1.0 + hp_weights(i);
+      aphys::MatxXd t(2, 3);
+      t = (A.block(i * 2, 0, 2, 3) + hp_weights[i] * hp_A) / weight_sum;
+      aphys::Vecxd x_tilde(3);
+      x_tilde << v_p_ref(i, 0), v_p_ref(i, 1), 1;
+      optimized_rig.row(i) = (t * x_tilde).transpose();
+    }
+  };
+
+  // ===================== prepare render data =====================
+  aphys::Points points = aphys::create_points();
+  points.point_size = 3.0f;
+
+  aphys::Points handle_points = aphys::create_points();
+  handle_points.color = aphys::RGB(0, 0, 0);
+  handle_points.point_size = 5.0f;
+
+  aphys::Edges edges = aphys::create_edges();
+  edges.color = aphys::RGB(255, 0, 0);
+  edges.width = 0.5f;
+
+  aphys::Lines force_lines = aphys::create_deriv_lines();
+  force_lines.color = aphys::RGB(220, 0, 131);
+  force_lines.width = 1.0f;
+
+  aphys::Points hp_points = aphys::create_points();
+  hp_points.color = aphys::RGB(0, 255, 0);
+  hp_points.point_size = 5.0f;
+
+  aphys::set_points_data(points, v_p.cast<float>(), aphys::MatxXf());
+  aphys::set_edges_data(edges, v_p.cast<float>(), edge_indices,
+                        aphys::MatxXf());
+  aphys::set_points_data(handle_points, handles.cast<float>(), aphys::MatxXf());
+
+  aphys::add_render_func(scene, aphys::get_render_func(handle_points));
+  aphys::add_render_func(scene, aphys::get_render_func(hp_points));
+  aphys::add_render_func(scene, aphys::get_render_func(points));
+  aphys::add_render_func(scene, aphys::get_render_func(force_lines));
+  aphys::add_render_func(scene, aphys::get_render_func(edges));
+
+  // ===================== handle input =====================
+  int curr_mode = 0;
+  aphys::add_gui_key_input_func(gui,
+                                [&curr_mode, &handles, &optimize_one_step]() {
+                                  if (ImGui::IsKeyPressed(ImGuiKey_H)) {
+                                    curr_mode = (curr_mode + 1) % 3;
+                                  }
+                                  if (ImGui::IsKeyPressed(ImGuiKey_R)) {
+                                    optimize_one_step();
+                                  }
+                                });
+
   // ===================== main loop =====================
   glfwSwapInterval(1);
 
@@ -212,15 +294,15 @@ int main() {
     set_handle_world_affine(T[0], handles.row(0));
     set_handle_world_affine(T[1], handles.row(1));
 
-    // lbs();
-    aphys::Vecxd v_vec = aphys::Vecxd::Map(v_p_ref.data(), v_p_ref.size());
+    lbs();
+    /*aphys::Vecxd v_vec = aphys::Vecxd::Map(v_p_ref.data(), v_p_ref.size());
     aphys::Vecxd T_vec(handles.rows() * 6);
     for (int j = 0; j < handles.rows(); j++) {
       aphys::Vecxd t_vec = aphys::Vecxd::Map(T[j].data(), T[j].size());
       T_vec.segment(j * 6, 6) = t_vec;
     }
     v_vec = lbs_W * T_vec;
-    v_p_rig = aphys::MatxXd::Map(v_vec.data(), v_p.rows(), 2);
+    v_p_rig = aphys::MatxXd::Map(v_vec.data(), v_p.rows(), 2);*/
 
     pd_solver.localStep(v_p_rig, face_indices);
     aphys::MatxXd tmp = pd_solver.J * pd_solver.P;
@@ -235,15 +317,23 @@ int main() {
 
     // aphys::set_deriv_lines_data(force_lines, v_p_rig.cast<float>(),
     //                             dx.cast<float>(), 1.0f, aphys::MatxXf());
-    aphys::set_points_data(points, v_p_rig.cast<float>(), aphys::MatxXf());
-    if (curr_mode == 0)
+    if (curr_mode == 0) {
+      aphys::set_points_data(points, v_p_rig.cast<float>(), aphys::MatxXf());
       aphys::set_edges_data(edges, v_p_rig.cast<float>(), edge_indices,
                             aphys::MatxXf());
-    else if (curr_mode == 1)
+    } else if (curr_mode == 1) {
+      aphys::set_points_data(points, dx.cast<float>(), aphys::MatxXf());
       aphys::set_edges_data(edges, dx.cast<float>(), edge_indices,
                             aphys::MatxXf());
+    } else if (curr_mode == 2) {
+      aphys::set_edges_data(edges, optimized_rig.cast<float>(), edge_indices,
+                            aphys::MatxXf());
+      aphys::set_points_data(points, optimized_rig.cast<float>(),
+                             hp_weight_color);
+    }
     aphys::set_points_data(handle_points, handles_rig.cast<float>(),
                            aphys::MatxXf());
+    aphys::set_points_data(hp_points, hp_pos.cast<float>(), aphys::MatxXf());
 
     aphys::render_scene(scene);
     aphys::render_gui(gui);
